72c2965ef3a623dc42aa889d1e1b7fbba9a1145b
Viviana Benitez <viviana.bernitez@gmail.com>
2022-10-18 11:52:50 -0500

    Actualizando comentarios

--- Test_conda_settings.py
+++ Test_conda_settings.py
@@ -2,15 +2,18 @@
 #print(sys.version)
 
 class Autos:
-    #este sería el setter
+    #este sería como el setter?
+    #Special method: constructor, main diff between functions and methods
+    #does not have a return value
     def __init__(self, marca, color):
         self.marca = marca
         self.color = color
 
-    #y este sería el getter
+    #Se comporta como el getter
     def show(self):
         return print("Brand", self.marca), print("Color", self.color)
 
+    #Método random
     def change(self):
         return print(self.marca,"cambiar a Toyota")
 
@@ -18,9 +21,11 @@ class Autos:
 marca = str(input("Enter car brand: "))
 color = str(input("Enter car color: "))
 auto1 = Autos(marca,color)
-auto2 = Autos("CHevrolet", "azul islandia")
+#instancia llamada auto2 de la clase Autos
+auto2 = Autos("Chevrolet", "azul islandia")
 #ahora motrar la interface del método show
 auto1.show()
 auto2.show()
+#return change method
 auto2.change()
 
